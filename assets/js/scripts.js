0ec49fb2052aff88ac17fbd446d886fe8bc6cec2
thiagopnts <thiagopnts@gmail.com>
2013-12-27 18:04:51 -0300

    using Simple Man as theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,7 +19,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     var vpH = $(window).height();
-    var vH = vpH - 350;
+    var vH = (vpH - 350);
+    vH = vH <= 356 ? vH : 356;
     $('.overlay').css("height", vH);
     $('.featured-image').css("height", vH);
 });
